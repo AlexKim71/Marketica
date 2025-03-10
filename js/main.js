1cf6a109dc3fe78185953d74b91c16659766267d
Sanchez Kim <gavrylovalexandr@gmail.com>
2025-03-10 21:32:47 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+
+    $('.photo__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/photo-slider/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/photo-slider/arrow-right.svg" alt=""></button>',
+    });
+
+});
+
